c2a0e58690538e9b782d6426d88dde6eb05038b5
roheka <roheka.app@gmail.com>
2023-07-28 22:19:39 -0400

    first commit

---
+++ app.config.ts
@@ -0,0 +1,62 @@
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+
+import { ClientEnv, Env } from './env';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: Env.NAME,
+  description: `${Env.NAME} Mobile App`,
+  slug: 'obytesapp',
+  version: Env.VERSION.toString(),
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'cover',
+    backgroundColor: '#F75469',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: Env.BUNDLE_ID,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+    package: Env.PACKAGE,
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: [
+    ['@bacons/link-assets', ['./assets/fonts/Inter.ttf']],
+    'expo-localization',
+    [
+      'app-icon-badge',
+      {
+        enabled: true,
+        badges: [
+          {
+            text: Env.APP_ENV,
+            type: 'banner',
+            color: 'white',
+          },
+          {
+            text: Env.VERSION.toString(),
+            type: 'ribbon',
+            color: 'white',
+          },
+        ],
+      },
+    ],
+  ],
+  extra: {
+    ...ClientEnv,
+  },
+});
